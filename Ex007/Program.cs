ae1fec9baa297016b722872a70d351a573a23b83
Andery Pahynov <proxendei12345q@gmail.com>
2023-01-18 22:58:03 +0300

    Ex007

---
+++ Ex007/Program.cs
@@ -0,0 +1,71 @@
+﻿// Алгоритм:
+
+// 1. Определить три точки
+// 2. Выбрат две любых
+// 3. Найти середину
+// 4. Поставить точку
+// 5. Выбрать случайную вершину треугольника
+// 6. Соеденить её с полученной на 4 шаге точкой
+// 7. Перейти к шагу 3
+// 8. Шаги 3 - 7 повторить 9, 28, 31 раз
+
+// Синтаксис начало:
+
+// while (Услови епредложения)
+// {
+//     Набор действий
+// }
+
+// int count = 0;
+
+// while(count < 100)
+// {
+//     Набор действий
+//     count = count + 1
+// }
+
+Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+
+int x =xa, y = xb;
+
+int count = 0;
+
+while(count < 10000)
+{
+    int what = new Random().Next(0, 3); // [0;3] 0 1 2
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count ++;
+}
