02ef80e0f3e9f0a19c2bf229d450228f2aaf0e89
Andery Pahynov <proxendei12345q@gmail.com>
2023-01-18 21:24:03 +0300

    ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("Введите ваше имя "); // в разные строки 
+string username = Console.ReadLine();
+Console.WriteLine("Привет,");
+Console.WriteLine(username);
+// System.Console.Write(); - выводит в одну строку
